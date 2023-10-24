73a40f75245d5d1dfc270b9c5dbc4dae88a2f5c8
Nitin338 <107560736+Nitin338@users.noreply.github.com>
2023-10-24 22:45:55 +0530

    Add Commented Code

--- starpaterns.js
+++ starpaterns.js
@@ -11,6 +11,16 @@ for(let i=1;i<=n;i++){
 console.log(ab);
 
 
+// let n=5;
+// let ab="";
+// for(let i=1;i<=n;i++){
+//   for(let j=1;j<=i;j++){
+//     ab +=i;
+
+//   }
+//   ab +="\n"
+// }
+// console.log(ab);
 
 
 // // let n=5;
