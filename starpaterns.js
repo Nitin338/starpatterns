e880ae7fe506e803f2c24509597e54dae5c4f8b0
Nitin338 <107560736+Nitin338@users.noreply.github.com>
2023-10-24 22:43:48 +0530

    Remove  Comment code

--- starpaterns.js
+++ starpaterns.js
@@ -1,31 +1,3 @@
-// for (i = 1; i <= 5; i++)
-//  {
-//     for (j = 1; j <= i; j++) 
-
-//     {
-//         console.log("*")
-//     }
-
-//     }
-    
-
-
-
-
-
-
-// let n=5;
-// let string= "";
-// for(let i=1;i<=n;i++){
-//   for(let j=0;j<i;j++){
-//     string += "*";
-
-//   }
-//   string += "\n";
-
-// }
-// console.log(string);
-
 
 let n=5;
 let ab="";
